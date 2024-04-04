76ce2ce644cb2496f71c3f1b75cd85fc64767b13
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2024-04-04 21:37:15 +0300

    Use API pointer in ModInfo for bind calls.

--- Source/GenerationsRaytracing.X86/FileBinder.cpp
+++ Source/GenerationsRaytracing.X86/FileBinder.cpp
@@ -4,19 +4,17 @@
 
 void FileBinder::init(ModInfo_t* modInfo)
 {
-    const auto api = GetModLoaderAPI();
-
     const char* sonicFurArchive = Configuration::s_furStyle == FurStyle::CG ? 
         "disk/bb3/SonicFurCG.arl" : "disk/bb3/SonicFurFrontiers.arl";
 
     const char* sonicClassicFurArchive = Configuration::s_furStyle == FurStyle::CG ?
         "disk/bb3/SonicClassicFurCG.arl" : "disk/bb3/SonicClassicFurFrontiers.arl";
 
-    api->BindFile(modInfo->CurrentMod, "+Title.arl", sonicFurArchive, 0);
-    api->BindFile(modInfo->CurrentMod, "+Sonic.arl", sonicFurArchive, 0);
-    api->BindFile(modInfo->CurrentMod, "+evSonic.arl", sonicFurArchive, 0);
+    modInfo->API->BindFile(modInfo->CurrentMod, "+Title.arl", sonicFurArchive, 0);
+    modInfo->API->BindFile(modInfo->CurrentMod, "+Sonic.arl", sonicFurArchive, 0);
+    modInfo->API->BindFile(modInfo->CurrentMod, "+evSonic.arl", sonicFurArchive, 0);
 
-    api->BindFile(modInfo->CurrentMod, "+Title.arl", sonicClassicFurArchive, 0);
-    api->BindFile(modInfo->CurrentMod, "+SonicClassic.arl", sonicClassicFurArchive, 0);
-    api->BindFile(modInfo->CurrentMod, "+evSonicClassic.arl", sonicClassicFurArchive, 0);
+    modInfo->API->BindFile(modInfo->CurrentMod, "+Title.arl", sonicClassicFurArchive, 0);
+    modInfo->API->BindFile(modInfo->CurrentMod, "+SonicClassic.arl", sonicClassicFurArchive, 0);
+    modInfo->API->BindFile(modInfo->CurrentMod, "+evSonicClassic.arl", sonicClassicFurArchive, 0);
 }
