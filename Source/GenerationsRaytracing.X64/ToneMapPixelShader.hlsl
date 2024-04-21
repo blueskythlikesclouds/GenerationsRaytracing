0d833d8f4d55aabf72a51f3b0e347a7b42ce7687
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2024-04-21 04:08:39 +0300

    Update tone map function to cover slightly more range.

--- Source/GenerationsRaytracing.X64/ToneMapPixelShader.hlsl
+++ Source/GenerationsRaytracing.X64/ToneMapPixelShader.hlsl
@@ -10,7 +10,16 @@ float4 main(float4 position : SV_Position, float4 texCoord : TEXCOORD) : SV_Targ
     SamplerState samplerState = SamplerDescriptorHeap[g_SamplerIndex];
     
     float3 rgb = texture.SampleLevel(samplerState, texCoord.xy, 0).rgb;
-    float3 crgb = 1.0 - exp2(1.76112 - 5.0 * rgb);
 
-    return float4(select(rgb >= 0.72974005284, crgb, rgb), 1.0);
+    const float grayPoint = 0.5;
+    const float whitePoint = 4.0;
+    const float curve = 2.0;
+    const float scale = (whitePoint - grayPoint) / pow(pow((1.0 - grayPoint) / (whitePoint - grayPoint), -curve) - 1.0, 1.0 / curve);
+    
+    float3 tonemappedRgb = (rgb - grayPoint) / scale;
+    tonemappedRgb = saturate(grayPoint + scale * tonemappedRgb / pow(1.0 + pow(tonemappedRgb, curve), 1.0 / curve));
+    
+    rgb = select(rgb >= grayPoint, tonemappedRgb, rgb);
+    
+    return float4(rgb, 1.0);
 }
