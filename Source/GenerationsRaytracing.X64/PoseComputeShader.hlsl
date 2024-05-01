26113d28e9b56530f47c12795ac35a123ecb151d
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2024-05-01 22:39:57 +0300

    Deactivate vertices if they get scaled to 0.

--- Source/GenerationsRaytracing.X64/PoseComputeShader.hlsl
+++ Source/GenerationsRaytracing.X64/PoseComputeShader.hlsl
@@ -39,6 +39,13 @@ float4x4 ComputeNodeMatrix(uint4 blendIndices, float4 blendWeight)
         g_Pose[g_Palette[blendIndices.w]] * blendWeight.w;
 }
 
+bool CheckNodeMatrixUnscaled(float4x4 nodeMatrix)
+{
+    return dot(nodeMatrix[0].xyz, nodeMatrix[0].xyz) == 0 ||
+        dot(nodeMatrix[1].xyz, nodeMatrix[1].xyz) == 0 ||
+        dot(nodeMatrix[2].xyz, nodeMatrix[2].xyz) == 0;
+}
+
 [numthreads(64, 1, 1)]
 void main(uint3 dispatchThreadId : SV_DispatchThreadID)
 {
@@ -47,10 +54,9 @@ void main(uint3 dispatchThreadId : SV_DispatchThreadID)
 
     uint vertexOffset = dispatchThreadId.x * g_VertexStride;
     uint prevVertexOffset = g_VertexCount * g_VertexStride + dispatchThreadId.x * 0xC;
-
-    g_Dest.Store3(prevVertexOffset, g_Dest.Load3(vertexOffset));
-
-    float3 position = 0.0;
+    
+    float3 position = float3(asfloat(0x7FFFFFFF), 0.0, 0.0);
+    uint3 prevPosition = g_Dest.Load3(vertexOffset);
     
     if (g_Visible)
     {
@@ -82,20 +88,24 @@ void main(uint3 dispatchThreadId : SV_DispatchThreadID)
             blendWeight.w = 1.0;
 
         nodeMatrix += ComputeNodeMatrix(blendIndices, blendWeight);
+        
+        if (!CheckNodeMatrixUnscaled(nodeMatrix))
+        {
+            position = g_Src.Load<float3>(vertexOffset);
+            uint3 nrmTgnBin = g_Src.Load3(vertexOffset + g_NormalOffset);
+            float3 normal = DecodeSnorm10(nrmTgnBin.x);
+            float3 tangent = DecodeSnorm10(nrmTgnBin.y);
+            float3 binormal = DecodeSnorm10(nrmTgnBin.z);
 
-        position = g_Src.Load<float3>(vertexOffset);
-        uint3 nrmTgnBin = g_Src.Load3(vertexOffset + g_NormalOffset);
-        float3 normal = DecodeSnorm10(nrmTgnBin.x);
-        float3 tangent = DecodeSnorm10(nrmTgnBin.y);
-        float3 binormal = DecodeSnorm10(nrmTgnBin.z);
+            position = mul(nodeMatrix, float4(position, 1.0)).xyz;
+            normal = normalize(mul(nodeMatrix, float4(normal, 0.0)).xyz);
+            tangent = normalize(mul(nodeMatrix, float4(tangent, 0.0)).xyz);
+            binormal = normalize(mul(nodeMatrix, float4(binormal, 0.0)).xyz);
 
-        position = mul(nodeMatrix, float4(position, 1.0)).xyz;
-        normal = normalize(mul(nodeMatrix, float4(normal, 0.0)).xyz);
-        tangent = normalize(mul(nodeMatrix, float4(tangent, 0.0)).xyz);
-        binormal = normalize(mul(nodeMatrix, float4(binormal, 0.0)).xyz);
-
-        g_Dest.Store3(vertexOffset + g_NormalOffset, uint3(EncodeSnorm10(normal), EncodeSnorm10(tangent), EncodeSnorm10(binormal)));
+            g_Dest.Store3(vertexOffset + g_NormalOffset, uint3(EncodeSnorm10(normal), EncodeSnorm10(tangent), EncodeSnorm10(binormal)));
+        }
     }
     
     g_Dest.Store<float3>(vertexOffset, position);
+    g_Dest.Store3(prevVertexOffset, isnan(asfloat(prevPosition.x)) ? asuint(position) : prevPosition);
 }
