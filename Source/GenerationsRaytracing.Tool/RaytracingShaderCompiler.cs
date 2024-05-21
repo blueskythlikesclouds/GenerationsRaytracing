cf889f25e70dbd17c2db0787a3ab82f247aa18d5
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2024-05-21 22:27:21 +0300

    Redirect Cloth to Common.

--- Source/GenerationsRaytracing.Tool/RaytracingShaderCompiler.cs
+++ Source/GenerationsRaytracing.Tool/RaytracingShaderCompiler.cs
@@ -707,7 +707,7 @@ public static class RaytracingShaderCompiler
         ("ChrSkinHalf_", RaytracingShader.ChrSkinHalf),
         ("ChrSkinIgnore_", RaytracingShader.ChrSkinIgnore),
         ("Cloak_", RaytracingShader.SysError),
-        ("Cloth_", RaytracingShader.SysError),
+        ("Cloth_", RaytracingShader.Common),
         ("Cloud_", RaytracingShader.Cloud),
         ("Common_", RaytracingShader.Common),
         ("Deformation_", RaytracingShader.SysError),
