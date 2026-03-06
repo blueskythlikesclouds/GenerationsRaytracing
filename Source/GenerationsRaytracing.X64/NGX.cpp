d1255ce571bf5c41f35d69a4338e29b738572585
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2026-03-07 02:00:08 +0300

    Enable DLSS-RR Preset E.

--- Source/GenerationsRaytracing.X64/NGX.cpp
+++ Source/GenerationsRaytracing.X64/NGX.cpp
@@ -56,6 +56,13 @@ NGX::NGX(const Device& device) : m_device(device)
 
     m_parameters->Set(NVSDK_NGX_Parameter_ResourceAllocCallback, resourceAllocCallback);
     m_parameters->Set(NVSDK_NGX_Parameter_ResourceReleaseCallback, resourceReleaseCallback);
+
+    m_parameters->Set(NVSDK_NGX_Parameter_RayReconstruction_Hint_Render_Preset_DLAA, NVSDK_NGX_RayReconstruction_Hint_Render_Preset_E);
+    m_parameters->Set(NVSDK_NGX_Parameter_RayReconstruction_Hint_Render_Preset_Quality, NVSDK_NGX_RayReconstruction_Hint_Render_Preset_E);
+    m_parameters->Set(NVSDK_NGX_Parameter_RayReconstruction_Hint_Render_Preset_Balanced, NVSDK_NGX_RayReconstruction_Hint_Render_Preset_E);
+    m_parameters->Set(NVSDK_NGX_Parameter_RayReconstruction_Hint_Render_Preset_Performance, NVSDK_NGX_RayReconstruction_Hint_Render_Preset_E);
+    m_parameters->Set(NVSDK_NGX_Parameter_RayReconstruction_Hint_Render_Preset_UltraPerformance, NVSDK_NGX_RayReconstruction_Hint_Render_Preset_E);
+    m_parameters->Set(NVSDK_NGX_Parameter_RayReconstruction_Hint_Render_Preset_UltraQuality, NVSDK_NGX_RayReconstruction_Hint_Render_Preset_E);
 }
 
 NGX::~NGX()
